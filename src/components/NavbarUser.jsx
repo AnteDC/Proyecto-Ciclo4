7ce6d0e894364a504047ef7702daecaaa34aab6b
victor_arias <torvicarias@gmail.com>
2021-11-13 20:03:55 -0500

    Se crean modales faltantes

--- src/components/NavbarUser.jsx
+++ src/components/NavbarUser.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Logo from '../images/icono.png';
 import { NavLink } from "react-router-dom";
 import ModalLogin from './modales/Login';
+import ModalContacto from './modales/Contactenos';
+import ModalEquipo from './modales/Equipo';
+import ModalOfertas from './modales/Ofertas';
 
 const NavbarUser = () => {
     return (
@@ -24,16 +27,16 @@ const NavbarUser = () => {
                                 <NavLink className="nav-link active" aria-current="page" to="/">Inicio</NavLink>
                             </li>
                             <li className="nav-item px-2">
-                                <NavLink className="nav-link" to="/">Servicios</NavLink>
+                                <NavLink className="nav-link" to="/servicios">Servicios</NavLink>
                             </li>
                             <li className="nav-item px-2">
-                                <a className="nav-link" href="#modalOfertas" data-bs-toggle="modal" data-bs-target="#modalOfertas">Ofertas</a>
+                                <div className="nav-link cursor"><ModalOfertas /></div>
                             </li>
-                            {/* <li className="nav-item px-2">
-                                <NavLink className="nav-link" to="/Portada">Noticias</NavLink>
-                            </li> */}
                             <li className="nav-item px-2">
-                                <a className="nav-link" href="#modalContacto" data-bs-toggle="modal" data-bs-target="#modalContacto">Contáctenos</a>
+                                <div className="nav-link cursor"><ModalEquipo /></div>
+                            </li>
+                            <li className="nav-item px-2">
+                                <div className="nav-link cursor"><ModalContacto /></div>
                             </li>
                         </ul>
                         <div className="ms-2">
@@ -42,117 +45,6 @@ const NavbarUser = () => {
                     </div>
                 </div>
             </nav>
-
-            {/* Modal ofertas */}
-            <div className="modal fade" id="modalOfertas" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header bg-light">
-                            <div className="modal-title d-flex align-items-center">
-                                <img alt='' src={Logo} height="30" className="pe-2" />
-                                <h4 className="m-0"><b>Ofertas</b></h4>
-                            </div>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-info" data-bs-dismiss="modal">Cerrar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Modal login */}
-            <div className="modal fade" id="modalLogin" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header bg-light">
-                            <div className="modal-title d-flex align-items-center">
-                                <img alt='' src={Logo} height="30" className="pe-2" />
-                                <h4 className="m-0"><b>Ingresar</b></h4>
-                            </div>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <form>
-                                <div className="mt-2 mb-3 d-flex justify-content-center">
-                                    <input type="text" id="login" class="bg-light fadeIn second text-center py-2" name="login" placeholder="Usuario" />
-                                </div>
-                                <div className="mb-2 d-flex justify-content-center">
-                                    <input type="password" id="password" class="bg-light fadeIn third text-center py-2" name="login" placeholder="Contraseña" />
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-success">Ingresar</button>
-                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Modal contacto */}
-            <div className="modal fade" id="modalContacto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header bg-light">
-                            <div className="modal-title d-flex align-items-center">
-                                <img alt='' src={Logo} height="30" className="pe-2" />
-                                <h4 className="m-0"><b>Contáctenos</b></h4>
-                            </div>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-info" data-bs-dismiss="modal">Cerrar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Modal equipo */}
-            <div className="modal fade" id="modalEquipo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div className="modal-content">
-                        <div className="modal-header bg-light">
-                            <div className="modal-title d-flex align-items-center">
-                                <img alt='' src={Logo} height="30" className="pe-2" />
-                                <h4 className="m-0"><b>Nuestro equipo</b></h4>
-                            </div>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                            <hr />
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                            <hr />
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                            <hr />
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                            <hr />
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eum quidem iste possimus accusamus minus corporis eos, illo voluptate rem laborum dolore voluptates! Tenetur dolorem fuga, et ad accusamus ullam!
-                            </p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-info" data-bs-dismiss="modal">Cerrar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
