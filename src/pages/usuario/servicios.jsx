ea03c5b4ca8380a8b22bd9b0f338603abdaa453b
victor_arias <torvicarias@gmail.com>
2021-11-22 20:03:39 -0500

    Se añaden cards temporales en la página de servicios

--- src/pages/usuario/servicios.jsx
+++ src/pages/usuario/servicios.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import NavbarUser from '../../components/NavbarUser';
-import FooterUser from '../../components/FooterUser';
+import React from "react";
+import NavbarUser from "../../components/NavbarUser";
+import FooterUser from "../../components/FooterUser";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Servicios = () => {
-    return (
-        <div>
-            <NavbarUser />
-                <div className="container cuerpo">
-                    <h1 className="py-3">Servicios</h1>
-                </div>
-            <FooterUser />
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavbarUser />
+      <div className="container cuerpo">
+        <h1 className="py-3">Servicios</h1>
+        <Row xs={1} md={2} className="g-4 mb-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card className="sombra">
+                <Card.Header>Categoría del servicio</Card.Header>
+                <Card.Body>
+                  <Card.Title>Nombre del servicio</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Precio del servicio ($)
+                  </Card.Subtitle>
+                  <Card.Text>Descripción del servicio</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <FooterUser />
+    </div>
+  );
+};
 
-export default Servicios
+export default Servicios;
